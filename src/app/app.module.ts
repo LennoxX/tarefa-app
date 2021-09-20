9ee7c6d4c30e5b0795dd5781b1961dc9b58739d1
Glacy <glacy.kelly3@gmail.com>
2021-09-20 18:46:55 -0300

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     RouterModule
--- src/app/pages/categoria/categoria-form/categoria-form.component.ts
+++ src/app/pages/categoria/categoria-form/categoria-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria-form',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria-form.component.css']
 })
 export class CategoriaFormComponent implements OnInit {
+  categoriaForm!: FormGroup
+  id!: FormControl
+  nome!: FormControl
 
   constructor() { }
 
   ngOnInit(): void {
+    this.id = new FormControl('')
+    this.nome = new FormControl('', Validators.required)
+    this.categoriaForm = new FormGroup({
+      id: this.id,
+      nome: this.nome,
+    })
+  }
+
+  salvarForm(){
+    console.log(this.categoriaForm.value)
   }
 
 }
